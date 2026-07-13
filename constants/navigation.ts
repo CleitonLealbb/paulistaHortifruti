2602a13fc179fa36758edbca028fc4408f69f671
cleitonleal_ofc <cleiton_nx@hotmail.com>
2026-07-13 18:05:21 -0300

    chore: organiza estrutura inicial do projeto

---
+++ constants/navigation.ts
@@ -0,0 +1,18 @@
+export const navigation = [
+  {
+    label: "Início",
+    href: "#home",
+  },
+  {
+    label: "Sobre",
+    href: "#sobre",
+  },
+  {
+    label: "Categorias",
+    href: "#categorias",
+  },
+  {
+    label: "Contato",
+    href: "#contato",
+  },
+];
---
+++ constants/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "Paulista Hortifruti",
+
+  slogan: "Do campo à sua mesa.",
+
+  description:
+    "Frutas, verduras e legumes sempre frescos.",
+
+  whatsapp: "https://wa.me/5511999999999",
+
+  phone: "(11) 99999-9999",
+
+  instagram: "#",
+
+  address: "Rua XXXX",
+
+  city: "Sua Cidade",
+};
